f0b208577bb5980f6b4729af3d27bb872d0ae959
Elena <elena@MacBook-Air-Elena.local>
2020-02-24 20:15:24 +0300

    init

--- lesson/1/exercise/2.py
+++
@@ -1,8 +0,0 @@
-var = int(input("Enter time in seconds: "))
-
-hours = var // (60*60)
-minutes = (var - (60*60*hours)) // 60
-seconds = var - (60*60*hours) - (60*minutes)
-
-print(f"time: {hours:02d}:{minutes:02d}:{seconds:02d}")
-
--- lesson/1/exercise/3.py
+++
@@ -1,3 +0,0 @@
-var = input("Enter number: ")
-print(f"Result: {int(var) + int(var*2) + int(var*3)}")
-
